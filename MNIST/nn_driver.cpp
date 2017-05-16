4d01acf3c7a93c637763a32e7061f244eae11fc2
aaddaamm30 <a.daman.loo@gmail.com>
2017-05-16 18:03:38 +0900

    top level main file. empty currently but will be formatted tomorrow

---
+++ MNIST/nn_driver.cpp
@@ -0,0 +1,17 @@
+/****************************************************************
+*
+*	File		: main.cpp
+*	Description	: This is the top level main program for the
+*				  MNIST dataset neural network. It take flags
+*				  that indicate if it is testing or training and
+*				  appropreate weight file input if using specific
+*				  pre trained weigths as well.
+*				  
+*	Author		: Adam Loo
+*	Last Edited	: Tue May 16 2017
+*
+****************************************************************/
+
+
+
+
